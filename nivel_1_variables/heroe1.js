3dcb26ddcee23e552fda06098342c96ab3e8f880
Yojan1409 <1561124@senati.pe>
2026-03-28 12:23:43 -0500

    Modificación de los héroes: nombre, poder, energía, ciudad y enemigo

--- nivel_1_variables/heroe1.js
+++ nivel_1_variables/heroe1.js
@@ -1,7 +1,7 @@
-let nombreHeroe = "Stark";
-let poder = "super fuerza, velocidad y alta resistencia";
-let nivelEnergia = 150;
-let ciudad = "Lugúnica";
-let enemigo = "Lye";
+let nombreHeroe = "Blue";
+let poder = "super inteligencia, velocidad y alta resistencia";
+let nivelEnergia = 170;
+let ciudad = "América";
+let enemigo = "Ryu";
 
 console.log(nombreHeroe + " un héroe con " + poder + ", originario de " + ciudad + ", está combatiendo contra " + enemigo + " con su energía al " + nivelEnergia + " por ciento.")
--- nivel_1_variables/heroe2.js
+++ nivel_1_variables/heroe2.js
@@ -1,8 +1,8 @@
-let nameHeroe = "Garfield";
+let nameHeroe = "Urano";
 let power = "invencibilidad, manipulación de la gravedad, así como super fuerza";
-let energia = 80;
-let lugar = "Pristella";
-let archienemigo = "Roswall";
+let energia = 10;
+let lugar = "Lima";
+let archienemigo = "Megatron";
 let amigo = "Stark";
 
 console.log(amigo + " y " + nameHeroe + " un antihéroe con " + power + " que viene de la ciudad de " + lugar + ", está por entrar a una guerra contra el supervilano " + archienemigo + ", aunque solo tiene el " + energia + " por ciento de su poder.")
