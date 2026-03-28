4989515e48d745e46c3011ead4e2f29c976428d6
Yojan1409 <1561124@senati.pe>
2026-03-28 11:28:23 -0500

    Estructura inicial del proyecto

---
+++ nivel_1_variables/heroe1.js
@@ -0,0 +1,7 @@
+let nombreHeroe = "Stark";
+let poder = "super fuerza, velocidad y alta resistencia";
+let nivelEnergia = 150;
+let ciudad = "Lugúnica";
+let enemigo = "Lye";
+
+console.log(nombreHeroe + " un héroe con " + poder + ", originario de " + ciudad + ", está combatiendo contra " + enemigo + " con su energía al " + nivelEnergia + " por ciento.")
---
+++ nivel_1_variables/heroe2.js
@@ -0,0 +1,8 @@
+let nameHeroe = "Garfield";
+let power = "invencibilidad, manipulación de la gravedad, así como super fuerza";
+let energia = 80;
+let lugar = "Pristella";
+let archienemigo = "Roswall";
+let amigo = "Stark";
+
+console.log(amigo + " y " + nameHeroe + " un antihéroe con " + power + " que viene de la ciudad de " + lugar + ", está por entrar a una guerra contra el supervilano " + archienemigo + ", aunque solo tiene el " + energia + " por ciento de su poder.")
