3900ad9edd14b5d59abc511a761e61abd5634626
Yojan1409 <1561124@senati.pe>
2026-03-31 10:25:08 -0500

    Se creó los objetos de jugador1 y jugador 2

---
+++ nivel_3_objetos/videojuego_rpg.js
@@ -0,0 +1,13 @@
+let jugador1 = {
+    nombre: "Xorus",
+    vida: 100,
+    fuerza: 80,
+    nivel: 50
+};
+
+let jugador2 = {
+    nombre: "Bell",
+    vida: 120,
+    fuerza: 100,
+    nivel: 80
+};
