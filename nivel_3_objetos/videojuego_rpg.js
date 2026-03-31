f8ba0122b0b80e9745bf2c866b66c181b22b34f0
Yojan1409 <1561124@senati.pe>
2026-03-31 11:24:21 -0500

    Función de entrenamiento añadida para ambos jugadores

--- nivel_3_objetos/videojuego_rpg.js
+++ nivel_3_objetos/videojuego_rpg.js
@@ -1,13 +1,25 @@
 let jugador1 = {
     nombre: "Xorus",
     vida: 100,
-    fuerza: 80,
-    nivel: 50
+    fuerza: 30,
+    nivel: 30
 };
 
 let jugador2 = {
     nombre: "Bell",
     vida: 120,
-    fuerza: 100,
-    nivel: 80
+    fuerza: 50,
+    nivel: 50
 };
+
+function entrenar(jugador1, jugador2) {
+    jugador1.fuerza += 15;
+    jugador2.vida -= 7;
+
+    jugador2.fuerza += 10;
+    jugador2.vida -= 5;
+
+    console.log(jugador1.nombre + " y " + jugador2.nombre + " entrenan duramente contra una dragón, siendo que ahora tienen " + jugador1.fuerza + " y " + jugador2.fuerza + " de fuerza, respectivamente.");
+}
+
+entrenar(jugador1, jugador2);
